80322aaed74c3982c69f4c938a35a10e6c418467
dms-vorawun <vorawun.k@dms.mail.go.th>
2025-06-13 11:32:08 +0700

    Update etl.py

---
+++ etl.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import match
+
+# E
+df = pd.read_csv(sample_product.csv)
+
+# T
+df["unit_price"] = df["unit_price"].apply(match.teil)
+
+# L
+df.to_csv("output/clern_produce.csv", index="False")
