6f624fa33679bdf233e862b24b432f49a25408cc
dms-vorawun <vorawun.k@dms.mail.go.th>
2025-06-13 13:07:23 +0700

    แก้โค้ด

--- etl.py
+++ etl.py
@@ -1,11 +1,6 @@
 import pandas as pd
 import math
 
-# E
-df = pd.read_csv(sample_products.csv)
-
-# T
-df["unit_price"] = df["unit_price"].apply(math.ceil)
-
-# L
-df.to_csv("output/clern_produce.csv", index="False")
+df = pd.read_csv('sample_products.csv')
+df['unit_price'] = df['unit_price'].apply(math.ceil)
+df.to_csv('output/cleaned_products.csv', index=False)
