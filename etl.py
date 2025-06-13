934ff2e6bcc3f2de3d0357d2b029ca1073665202
dms-vorawun <vorawun.k@dms.mail.go.th>
2025-06-13 11:35:40 +0700

    Update etl.py

--- etl.py
+++ etl.py
@@ -5,7 +5,7 @@ import match
 df = pd.read_csv(sample_product.csv)
 
 # T
-df["unit_price"] = df["unit_price"].apply(match.teil)
+df["unit_price"] = df["unit_price"].apply(match.ceil)
 
 # L
 df.to_csv("output/clern_produce.csv", index="False")
